a67c7009f99ce06845916026a3c3429c0461c85b
HerbertNtim <herbertntim15@gmail.com>
2024-08-10 04:25:59 +0000

    watch page done

--- frontend/src/pages/WatchPage.jsx
+++ frontend/src/pages/WatchPage.jsx
@@ -1,7 +1,20 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 import { useContentStore } from "../store/content";
 import axios from "axios";
+import ReactPlayer from "react-player";
+import Navbar from "../components/Navbar";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import { ORIGINAL_IMG_BASE_URL, SMALL_IMG_BASE_URL } from "../utils/constants";
+import WatchPageSkeleton from "../components/skeletons/WatchPageSkeleton";
+
+function formatReleaseDate(date) {
+  return new Date(date).toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+}
 
 const WatchPage = () => {
   const [trailers, setTrailers] = useState([]);
@@ -12,19 +25,38 @@ const WatchPage = () => {
 
   const { id } = useParams();
   const { contentType } = useContentStore();
+  const sliderRef = useRef(null);
 
-  console.log("contentType:", contentType, "id:", id);
+  const handleNext = () => {
+    if (currentTrailerIdx < trailers.length - 1) {
+      setCurrentTrailerIdx(currentTrailerIdx + 1);
+    }
+  };
 
+  const handlePrev = () => {
+    if (currentTrailerIdx > 0) {
+      setCurrentTrailerIdx(currentTrailerIdx - 1);
+    }
+  };
+
+  const scrollLeft = () => {
+		if (sliderRef.current) sliderRef.current.scrollBy({ left: -sliderRef.current.offsetWidth, behavior: "smooth" });
+	};
+
+	const scrollRight = () => {
+		if (sliderRef.current) sliderRef.current.scrollBy({ left: sliderRef.current.offsetWidth, behavior: "smooth" });
+	};
 
   useEffect(() => {
     const getTrailers = async () => {
       try {
-        const res = await axios.get(
-          `/netview/${contentType}/${id}/trailers`
-        );
+        const res = await axios.get(`/netview/${contentType}/${id}/trailers`);
         setTrailers(res.data.trailers);
       } catch (error) {
-        console.error("Error fetching trailers:", error.response || error.message);
+        console.error(
+          "Error fetching trailers:",
+          error.response || error.message
+        );
         if (error.message.includes("404")) {
           setTrailers([]);
         }
@@ -37,12 +69,13 @@ const WatchPage = () => {
   useEffect(() => {
     const getSimilarContent = async () => {
       try {
-        const res = await axios.get(
-          `/netview/${contentType}/${id}/similar`
-        );
+        const res = await axios.get(`/netview/${contentType}/${id}/similar`);
         setSimilarContent(res.data.similarMovies);
       } catch (error) {
-        console.error("Error fetching similar content:", error.response || error.message);
+        console.error(
+          "Error fetching similar content:",
+          error.response || error.message
+        );
         if (error.message.includes("404")) {
           setTrailers([]);
         }
@@ -55,22 +88,164 @@ const WatchPage = () => {
   useEffect(() => {
     const getDetails = async () => {
       try {
-        const res = await axios.get(
-          `/netview/${contentType}/${id}/details`
-        );
+        const res = await axios.get(`/netview/${contentType}/${id}/details`);
         setContent(res.data.details);
       } catch (error) {
-        console.error("Error fetching details:", error.response || error.message);
+        console.error(
+          "Error fetching details:",
+          error.response || error.message
+        );
         if (error.message.includes("404")) {
           setContent([]);
         }
+      } finally{
+        setLoading(false)
       }
     };
 
     getDetails();
   }, [contentType, id]);
 
-  return <div>WatchPage</div>;
+  if (loading){
+    return (
+			<div className='min-h-screen bg-black p-10'>
+				<WatchPageSkeleton />
+			</div>
+		);
+  }
+
+	if (!content) {
+		return (
+			<div className='bg-black text-white h-screen'>
+				<div className='max-w-6xl mx-auto'>
+					<Navbar />
+					<div className='text-center mx-auto px-4 py-8 h-full mt-40'>
+						<h2 className='text-2xl sm:text-5xl font-bold text-balance'>Content not found 😥</h2>
+					</div>
+				</div>
+			</div>
+		);
+	}
+
+  return (
+    <section className="bg-black min-h-screen text-white">
+      <div className="mx-auto container h-full px-4 py-8">
+        <Navbar />
+
+        {trailers.length > 0 && (
+          <div className="flex justify-between items-start">
+            <button
+              className={`bg-gray-500/70 hover:bg-gray-500 text-white rounded px-4 py-2 ${
+                currentTrailerIdx === 0 ? "opacity-50 cursor-not-allowed" : ""
+              }`}
+              disabled={currentTrailerIdx === 0}
+              onClick={handlePrev}
+            >
+              <ChevronLeft size={24} />
+            </button>
+
+            <button
+              className={`bg-gray-500/70 hover:bg-gray-500 text-white rounded px-4 py-2 ${
+                currentTrailerIdx === trailers.length - 1
+                  ? "opacity-50 cursor-not-allowed"
+                  : ""
+              }`}
+              disabled={currentTrailerIdx === trailers.length - 1}
+              onClick={handleNext}
+            >
+              <ChevronRight size={24} />
+            </button>
+          </div>
+        )}
+
+        {trailers.length > 0 && (
+          <ReactPlayer
+            controls={true}
+            width={"100%"}
+            height={"70vh"}
+            className="mx-auto overflow-hidden rounded-lg"
+            url={`https://www.youtube.com/watch?v=${trailers[currentTrailerIdx].key}`}
+          />
+        )}
+
+        {trailers.length === 0 && (
+          <h2 className="text-xl text-center mt-5">
+            No trailers available for{" "}
+            <span className="font-bold text-red-600">
+              {content?.name || content?.title}
+            </span>{" "}
+            😒
+          </h2>
+        )}
+
+        {/* movie details */}
+        <div
+          className="flex flex-col md:flex-row items-center justify-between gap-20
+				max-w-6xl mx-auto pt-4"
+        >
+          <div className="mb-4 md:mb-0">
+            <h2 className="text-5xl font-bold text-balance">
+              {content?.title || content?.name}
+            </h2>
+
+            <p className="mt-2 text-lg">
+              {formatReleaseDate(
+                content?.release_date || content?.first_air_date
+              )}{" "}
+              |{" "}
+              {content?.adult ? (
+                <span className="text-red-600">18+</span>
+              ) : (
+                <span className="text-green-600">PG-13</span>
+              )}{" "}
+            </p>
+            <p className="mt-4 text-lg">{content?.overview}</p>
+          </div>
+          <img
+            src={ORIGINAL_IMG_BASE_URL + content?.poster_path}
+            alt="Poster image"
+            className="max-h-[500px] w-[500px] rounded-md flex-1 object-contain py-6"
+          />
+        </div>
+
+        {/* Similar movies */}
+        {similarContent.length > 0 && (
+					<div className='mt-12 max-w-5xl mx-auto relative'>
+						<h3 className='text-3xl font-bold mb-4'>Similar Movies/Tv Show</h3>
+
+						<div className='flex overflow-x-scroll scrollbar-hide gap-4 pb-4 group' ref={sliderRef}>
+							{similarContent.map((content) => {
+								if (content.poster_path === null) return null;
+								return (
+									<Link key={content.id} to={`/watch/${content.id}`} className='w-52 flex-none'>
+										<img
+											src={SMALL_IMG_BASE_URL + content.poster_path}
+											alt='Poster path'
+											className='w-full h-auto rounded-md'
+										/>
+										<h4 className='mt-2 text-lg font-semibold'>{content.title || content.name}</h4>
+									</Link>
+								);
+							})}
+
+							<ChevronRight
+								className='absolute top-1/2 -translate-y-1/2 right-2 w-8 h-8
+										opacity-0 group-hover:opacity-100 transition-all duration-300 cursor-pointer
+										 bg-red-600 text-white rounded-full'
+								onClick={scrollRight}
+							/>
+							<ChevronLeft
+								className='absolute top-1/2 -translate-y-1/2 left-2 w-8 h-8 opacity-0 
+								group-hover:opacity-100 transition-all duration-300 cursor-pointer bg-red-600 
+								text-white rounded-full'
+								onClick={scrollLeft}
+							/>
+						</div>
+					</div>
+				)}
+      </div>
+    </section>
+  );
 };
 
 export default WatchPage;
